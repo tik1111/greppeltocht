6b9d48cb244f79fb3114a4be6733110301c093f1
Sander Abbink <sanderabbink@gmail.com>
2024-04-30 08:43:04 +0200

    Login pagina

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
-<!DOCTYPE html>
-<html lang="nl">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Greppeltocht</title>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
-  </head>
-  <body>
-    <main>
-        <h1>Login pagina</h1>  
-    </main>
-	<script src="index.js"></script>
-  </body>
-</html>
+<?php include_once('header.php'); ?>
+
+<main>
+  <div class="container-login">
+    <h1>Inloggen</h1>
+
+    <form action="">
+      <input type="text" name="username" placeholder="Gebruikersnaam">
+      <input type="password" name="password" placeholder="Wachtwoord">
+
+      <button type="submit">
+        Inloggen
+      </button>
+    </form>
+  </div>
+
+</main>
+
+<?php include_once('footer.php'); ?>
