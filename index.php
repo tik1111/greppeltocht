0069fffb301c00c6a1c88a07f33f471b33851818
Sander Abbink <sanderabbink@gmail.com>
2024-06-01 22:17:19 +0200

    Login script

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once('header.php'); ?>
+<?php include_once('asset/header.php'); ?>
 
 <main>
   <div class="container-login">
@@ -18,4 +18,4 @@
   <img src="/img/mario.webp" />
 </main>
 
-<?php include_once('footer.php'); ?>
+<?php include_once('asset/footer.php'); ?>
