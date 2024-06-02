0d3b87e5b2ad17c01b0c702f5c60addeab5b2e2f
Sander Abbink <sanderabbink@gmail.com>
2024-06-02 21:06:14 +0200

    Kaart toevoegen

--- admin/index.js
+++ admin/index.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
   var map = L.map("map").setView([51.505, -0.09], 13);
 
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  L.tileLayer("https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png", {
+    maxZoom: 20,
+    attribution: "",
   }).addTo(map);
 });
