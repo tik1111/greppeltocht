b423f7ec32566a7dedd02c1df5eef49d580935e2
Sander Abbink <sanderabbink@gmail.com>
2024-06-03 09:32:07 +0200

    WIP

--- admin/index.js
+++ admin/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   var map = L.map("map").setView([52.14481352353405, 6.394056284204322], 13);
 
-  L.tileLayer("https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png", {
-    maxZoom: 20,
-    attribution: "",
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 });
