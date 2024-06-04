83aa08f4d4c7baf8629710bea17dae847ec86366
Sander Abbink <sanderabbink@gmail.com>
2024-06-04 11:24:45 +0200

    QR reader

---
+++ admin/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  // var map = L.map("map").setView([52.14481352353405, 6.394056284204322], 13);
+
+  // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  //   attribution:
+  //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  // }).addTo(map);
+
+  // bind all elements of a given class
+  $(".qrcode-reader").qrCodeReader();
+});
--- admin/tasks.php
+++ admin/tasks.php
@@ -3,8 +3,21 @@
 <main>
     <h1>Opdrachten</h1>
 
-    <div style="width: 500px" id="reader">
-    </div>
+    <form action="">
+        <input id="single" type="text" size="50">
+
+        <button type="button" class="qrcode-reader" id="openreader-single"
+            data-qrr-target="#single"
+            data-qrr-audio-feedback="false"
+            data-qrr-qrcode-regexp="^https?:\/\/">
+            Read QRCode
+        </button>
+
+    </form>
+
+    <!-- https://blog.minhazav.dev/research/html5-qrcode -->
+
+    <!-- https://github.com/mauntrelio/qrcode-reader -->
 </main>
 
 <?php include_once('layout/footer.php'); ?>
