76fac9a9483905324a0267fea61872d9517f797d
Sander Abbink <sanderabbink@gmail.com>
2024-06-18 19:28:53 +0200

    QR code

--- admin/tasks.php
+++ admin/tasks.php
@@ -20,6 +20,8 @@
         <!-- https://blog.minhazav.dev/research/html5-qrcode -->
 
         <!-- https://github.com/mauntrelio/qrcode-reader -->
+
+        <img src="includes/qrcode.php?s=qr&d=HELLO%20WORLD&sf=8&ms=r&md=0.8">
     </div>
 </main>
 
