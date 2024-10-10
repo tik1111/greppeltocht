e6880389fc65d753febf7ff1e838471b2019519d
Sander Abbink <sanderabbink@gmail.com>
2024-10-10 17:48:52 +0200

    Current location

--- admin/layout/footer.php
+++ admin/layout/footer.php
@@ -2,6 +2,10 @@
 
     <?php include_once('popups/qrcode.php'); ?>
 
+
+    <input type="text" class="latitude" />
+    <input type="text" class="longitude" />
+
     <script src="/admin/js/qrcode-reader.js?v=20190604"></script>
     <script src="/admin/js/index.js"></script>
   </body>
