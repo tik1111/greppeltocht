92ecad2db05c31a03e13a35ff6d39afc52f3fdfe
Sander Abbink <sanderabbink@gmail.com>
2024-10-10 21:19:30 +0200

    Confetti

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -22,6 +22,8 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
+    <script src="https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js"></script>
+
     <link rel="manifest" href="../manifest.json">
   </head>
   <body>
